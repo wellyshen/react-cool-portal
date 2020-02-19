939f8a073291dacdb08d49ddb9f17df3ffcda3c6
Welly Shen <hivoid19@gmail.com>
2020-02-19 16:34:27 +0800

    Feat: dynamic create root element when needed

--- src/createPortal.ts
+++ src/createPortal.ts
@@ -1,5 +1,36 @@
+import { SFC, ReactNode, ReactPortal, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export default (id: string) => ({ children }) => {
-  return createPortal()
+const createEl = (id: string): HTMLDivElement => {
+  const el = document.createElement('div');
+  el.setAttribute('id', id);
+  document.body.appendChild(el);
+
+  return el;
+};
+
+export interface Props {
+  children: ReactNode;
 }
+
+export default (containerId?: string): SFC<Props> => ({
+  children
+}: Props): ReactPortal => {
+  const [container, setContainer] = useState(null);
+
+  useEffect(() => {
+    const id = containerId || 'react-cool-portal';
+    setContainer(document.getElementById(id) || createEl(id));
+
+    return (): void => {
+      if (!container) return;
+
+      const timer = setTimeout(() => {
+        clearTimeout(timer);
+        if (container.innerHTML === '') container.remove();
+      }, 100);
+    };
+  }, [container]);
+
+  return container && createPortal(children, container);
+};
