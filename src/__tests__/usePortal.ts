a5de5ee8b3220bddd95ca7f0dadbc558a0aa11af
Welly Shen <hivoid19@gmail.com>
2020-02-21 12:17:53 +0800

    Test: setup basic test

---
+++ src/__tests__/usePortal.ts
@@ -0,0 +1,9 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import usePortal from '..';
+
+describe('usePortal', () => {
+  it('should...', () => {
+    renderHook(() => usePortal);
+  });
+});
