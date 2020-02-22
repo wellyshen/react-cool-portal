7c7b91224f3f6cf23a5833a64f96ec0d5a63eacb
Welly Shen <hivoid19@gmail.com>
2020-02-22 16:42:54 +0800

    Feat: add onShow/onHide features

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,76 @@
-import { SFC, useState, useMemo, useCallback } from 'react';
+import {
+  SFC,
+  SyntheticEvent,
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback
+} from 'react';
 
 import createPortal, { Props } from './createPortal';
 
+type Fn = (event?: SyntheticEvent) => void;
 interface Args {
   containerId?: string;
+  onShow?: Fn;
+  onHide?: Fn;
 }
 interface Return {
   Portal: SFC<Props>;
   isShow: boolean;
-  show: () => void;
-  hide: () => void;
-  toggle: () => void;
+  show: Fn;
+  hide: Fn;
+  toggle: Fn;
 }
 
 const usePortal = ({
-  containerId = 'react-cool-portal'
+  containerId = 'react-cool-portal',
+  onShow,
+  onHide
 }: Args = {}): Return => {
   const [isShow, setIsShow] = useState(true);
+  const onShowRef = useRef(null);
+  const onHideRef = useRef(null);
+
+  useEffect(() => {
+    if (onShow) onShowRef.current = onShow;
+    if (onHide) onHideRef.current = onHide;
+  }, [onShow, onHide]);
 
   const Portal = useMemo(() => createPortal(containerId, isShow), [
     containerId,
     isShow
   ]);
 
-  const show = useCallback((): void => {
-    setIsShow(true);
-  }, []);
+  const show: Fn = useCallback(
+    e => {
+      if (isShow) return;
 
-  const hide = useCallback((): void => {
-    setIsShow(false);
-  }, []);
+      setIsShow(true);
+      if (onShow) onShowRef.current(e);
+    },
+    [isShow, onShow]
+  );
 
-  const toggle = useCallback((): void => {
-    setIsShow(!isShow);
-  }, [isShow]);
+  const hide: Fn = useCallback(
+    e => {
+      if (!isShow) return;
+
+      setIsShow(false);
+      if (onHide) onHideRef.current(e);
+    },
+    [isShow, onHide]
+  );
+
+  const toggle: Fn = useCallback(
+    e => {
+      setIsShow(!isShow);
+      if (onShow && !isShow) onShowRef.current(e);
+      if (onHide && isShow) onHideRef.current(e);
+    },
+    [isShow, onShow, onHide]
+  );
 
   return { Portal, isShow, show, hide, toggle };
 };
