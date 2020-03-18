3409fe3eddea79a8bbfd89d9462c52ce9282a641
Welly Shen <hivoid19@gmail.com>
2020-03-18 18:02:22 +0800

    test: add more testing cases

--- src/__tests__/createPortal.tsx
+++ src/__tests__/createPortal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import createPortal from '../createPortal';
 
 describe('createPortal', () => {
-  const defaultId = 'react-cool-portal';
+  const rootId = 'react-cool-portal';
+  const childId = 'test';
 
   interface Args {
     containerId?: string;
@@ -14,22 +15,24 @@ describe('createPortal', () => {
     clickOutsideCb: Function;
     escCb: Function;
     baseElement: any;
+    getByTestId: Function;
+    unmount: Function;
   }
 
   const renderHelper = ({
-    containerId = defaultId,
+    containerId = rootId,
     isShow = true
   }: Args = {}): Return => {
     const clickOutsideCb = jest.fn();
     const escCb = jest.fn();
     const Portal = createPortal(containerId, isShow, clickOutsideCb, escCb);
-    const { baseElement } = render(
+    const { baseElement, getByTestId, unmount } = render(
       <Portal>
-        <div>Test</div>
+        <div data-testid={childId}>Test</div>
       </Portal>
     );
 
-    return { clickOutsideCb, escCb, baseElement };
+    return { clickOutsideCb, escCb, baseElement, getByTestId, unmount };
   };
 
   it('should render correctly', () => {
@@ -40,5 +43,26 @@ describe('createPortal', () => {
   it('should not render child', () => {
     const { baseElement } = renderHelper({ isShow: false });
     expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should auto remove root', () => {
+    jest.useFakeTimers();
+
+    const { baseElement, unmount } = renderHelper();
+    unmount();
+    jest.runAllTimers();
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should trigger callback when clicks outside of the child', () => {
+    const { clickOutsideCb } = renderHelper();
+    fireEvent.click(document);
+    expect(clickOutsideCb).toBeCalled();
+  });
+
+  it('should not trigger callback when clicks inside of the child', () => {
+    const { getByTestId, clickOutsideCb } = renderHelper();
+    fireEvent.click(getByTestId(childId));
+    expect(clickOutsideCb).not.toBeCalled();
   });
 });
